0db389ccdac89b5fa483b07eaabca0e4a434d5c1
j-pong <ljh93ljh@gmail.com>
2021-08-01 15:46:54 +0900

    new type of hynet2

--- egs/librispeech/asr2/run.sh
+++ egs/librispeech/asr2/run.sh
@@ -1,35 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
 set -e
 set -u
 set -o pipefail
 
-train_set="train_clean_100"
-train_pseudo_set="train_860"
+train_set="train_960"
 valid_set="dev"
-test_sets="test_clean"
+test_sets="test_clean test_other dev_clean dev_other"
 
-asr_config=conf/tuning/train_asr_wav2vec_ctc_semi.yaml
+asr_config=conf/tuning/train_asr_conformer7_n_fft512_hop_length256.yaml
 lm_config=conf/tuning/train_lm_transformer2.yaml
 inference_config=conf/decode_asr.yaml
 
-# --lm_config "${lm_config}" \
-# --lm_train_text "data/${train_set}/text data/local/other_text/text" \
-
 ./asr.sh \
-    --audio_format flac.ark \
     --lang en \
-    --ngpu 4 \
-    --token_type char \
+    --ngpu 16 \
+    --nbpe 5000 \
     --max_wav_duration 30 \
+    --speed_perturb_factors "0.9 1.0 1.1" \
     --asr_config "${asr_config}" \
-    --use_lm false \
     --lm_config "${lm_config}" \
-    --lm_train_text "data/train_960/text data/local/other_text/text" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
-    --train_pseudo_set "${train_pseudo_set}" \
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
-    --feats_normalize fair_like_norm "$@"
+    --lm_train_text "data/${train_set}/text data/local/other_text/text" \
+    --bpe_train_text "data/${train_set}/text" "$@"
